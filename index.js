3aafcb5c1639437bd93d6398b2d861c66e1a9842
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2021-07-05 21:06:44 +0200

    idfk

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 /*! fs-chunk-store. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
-module.exports = Storage
-
 const fs = require('fs')
 const os = require('os')
 const parallel = require('run-parallel')
@@ -17,235 +15,222 @@ try {
   TMP = os.tmpdir()
 }
 
-function Storage (chunkLength, opts) {
-  const self = this
-  if (!(self instanceof Storage)) return new Storage(chunkLength, opts)
-  if (!opts) opts = {}
+class Storage {
+  constructor (chunkLength, opts = {}) {
+    this.chunkLength = Number(chunkLength)
+    if (!this.chunkLength) throw new Error('First argument must be a chunk length')
+    this.path = opts.path
 
-  self.chunkLength = Number(chunkLength)
-  if (!self.chunkLength) throw new Error('First argument must be a chunk length')
-  self.path = opts.path
-
-  if (opts.files) {
-    if (!Array.isArray(opts.files)) {
-      throw new Error('`files` option must be an array')
-    }
-    self.files = opts.files.slice(0).map(function (file, i, files) {
-      if (file.path == null) throw new Error('File is missing `path` property')
-      if (file.length == null) throw new Error('File is missing `length` property')
-      if (file.offset == null) {
-        if (i === 0) {
-          file.offset = 0
-        } else {
-          const prevFile = files[i - 1]
-          file.offset = prevFile.offset + prevFile.length
-        }
-      }
-      if (self.path) file.path = path.resolve(path.join(self.path, file.path))
-      return file
-    })
-    self.length = self.files.reduce(function (sum, file) { return sum + file.length }, 0)
-    if (opts.length != null && opts.length !== self.length) {
-      throw new Error('total `files` length is not equal to explicit `length` option')
-    }
-  } else {
-    const len = Number(opts.length) || Infinity
-    if (!self.path) self.path = path.resolve(path.join(TMP, 'fs-chunk-store', randombytes(20).toString('hex')))
-    self.files = [{
-      offset: 0,
-      path: self.path,
-      length: len
-    }]
-    self.length = len
-  }
-
-  self.chunkMap = []
-  self.closed = false
-
-  self.files.forEach(function (file) {
-    file.open = thunky(function (cb) {
-      if (self.closed) return cb(new Error('Storage is closed'))
-      fs.mkdir(path.dirname(file.path), { recursive: true }, function (err) {
-        if (err) return cb(err)
-        if (self.closed) return cb(new Error('Storage is closed'))
-        cb(null, raf(file.path))
-      })
-    })
-  })
-
-  // If the length is Infinity (i.e. a length was not specified) then the store will
-  // automatically grow.
-
-  if (self.length !== Infinity) {
-    self.lastChunkLength = (self.length % self.chunkLength) || self.chunkLength
-    self.lastChunkIndex = Math.ceil(self.length / self.chunkLength) - 1
-
-    self.files.forEach(function (file) {
-      const fileStart = file.offset
-      const fileEnd = file.offset + file.length
-
-      const firstChunk = Math.floor(fileStart / self.chunkLength)
-      const lastChunk = Math.floor((fileEnd - 1) / self.chunkLength)
-
-      for (let p = firstChunk; p <= lastChunk; ++p) {
-        const chunkStart = p * self.chunkLength
-        const chunkEnd = chunkStart + self.chunkLength
-
-        const from = (fileStart < chunkStart) ? 0 : fileStart - chunkStart
-        const to = (fileEnd > chunkEnd) ? self.chunkLength : fileEnd - chunkStart
-        const offset = (fileStart > chunkStart) ? 0 : chunkStart - fileStart
-
-        if (!self.chunkMap[p]) self.chunkMap[p] = []
-
-        self.chunkMap[p].push({
-          from,
-          to,
-          offset,
-          file
-        })
-      }
-    })
-  }
-}
-
-Storage.prototype.put = function (index, buf, cb) {
-  const self = this
-  if (typeof cb !== 'function') cb = noop
-  if (self.closed) return nextTick(cb, new Error('Storage is closed'))
-
-  const isLastChunk = (index === self.lastChunkIndex)
-  if (isLastChunk && buf.length !== self.lastChunkLength) {
-    return nextTick(cb, new Error('Last chunk length must be ' + self.lastChunkLength))
-  }
-  if (!isLastChunk && buf.length !== self.chunkLength) {
-    return nextTick(cb, new Error('Chunk length must be ' + self.chunkLength))
-  }
-
-  if (self.length === Infinity) {
-    self.files[0].open(function (err, file) {
-      if (err) return cb(err)
-      file.write(index * self.chunkLength, buf, cb)
-    })
-  } else {
-    const targets = self.chunkMap[index]
-    if (!targets) return nextTick(cb, new Error('no files matching the request range'))
-    const tasks = targets.map(function (target) {
-      return function (cb) {
-        target.file.open(function (err, file) {
-          if (err) return cb(err)
-          file.write(target.offset, buf.slice(target.from, target.to), cb)
-        })
-      }
-    })
-    parallel(tasks, cb)
-  }
-}
-
-Storage.prototype.get = function (index, opts, cb) {
-  const self = this
-  if (typeof opts === 'function') return self.get(index, null, opts)
-  if (self.closed) return nextTick(cb, new Error('Storage is closed'))
-
-  const chunkLength = (index === self.lastChunkIndex)
-    ? self.lastChunkLength
-    : self.chunkLength
-
-  const rangeFrom = (opts && opts.offset) || 0
-  const rangeTo = (opts && opts.length) ? rangeFrom + opts.length : chunkLength
-
-  if (rangeFrom < 0 || rangeFrom < 0 || rangeTo > chunkLength) {
-    return nextTick(cb, new Error('Invalid offset and/or length'))
-  }
-
-  if (self.length === Infinity) {
-    if (rangeFrom === rangeTo) return nextTick(cb, null, Buffer.from(0))
-    self.files[0].open(function (err, file) {
-      if (err) return cb(err)
-      const offset = (index * self.chunkLength) + rangeFrom
-      file.read(offset, rangeTo - rangeFrom, cb)
-    })
-  } else {
-    let targets = self.chunkMap[index]
-    if (!targets) return nextTick(cb, new Error('no files matching the request range'))
-    if (opts) {
-      targets = targets.filter(function (target) {
-        return target.to > rangeFrom && target.from < rangeTo
-      })
-      if (targets.length === 0) {
-        return nextTick(cb, new Error('no files matching the requested range'))
-      }
-    }
-    if (rangeFrom === rangeTo) return nextTick(cb, null, Buffer.from(0))
-
-    const tasks = targets.map(function (target) {
-      return function (cb) {
-        let from = target.from
-        let to = target.to
-        let offset = target.offset
-
-        if (opts) {
-          if (to > rangeTo) to = rangeTo
-          if (from < rangeFrom) {
-            offset += (rangeFrom - from)
-            from = rangeFrom
+    if (opts.files) {
+      if (opts.files.constructor !== Array) throw new Error('`files` option must be an array')
+      this.files = opts.files.slice(0).map((file, i, files) => {
+        if (file.path == null) throw new Error('File is missing `path` property')
+        if (file.length == null) throw new Error('File is missing `length` property')
+        if (file.offset == null) {
+          if (i === 0) {
+            file.offset = 0
+          } else {
+            const prevFile = files[i - 1]
+            file.offset = prevFile.offset + prevFile.length
           }
         }
+        if (this.path) file.path = path.resolve(path.join(this.path, file.path))
+        return file
+      })
+      this.length = this.files.reduce((sum, file) => sum + file.length, 0)
+      if (opts.length != null && opts.length !== this.length) throw new Error('total `files` length is not equal to explicit `length` option')
+    } else {
+      const len = Number(opts.length) || Infinity
+      if (!this.path) this.path = path.resolve(path.join(TMP, 'fs-chunk-store', randombytes(20).toString('hex')))
+      this.files = [{
+        offset: 0,
+        path: this.path,
+        length: len
+      }]
+      this.length = len
+    }
 
-        target.file.open(function (err, file) {
+    this.chunkMap = []
+    this.closed = false
+
+    this.files.forEach(file => {
+      file.open = thunky(cb => {
+        if (this.closed) return cb(new Error('Storage is closed'))
+        fs.mkdir(path.dirname(file.path), { recursive: true }, err => {
           if (err) return cb(err)
-          file.read(offset, to - from, cb)
+          if (this.closed) return cb(new Error('Storage is closed'))
+          cb(null, raf(file.path))
         })
-      }
+      })
     })
 
-    parallel(tasks, function (err, buffers) {
-      if (err) return cb(err)
-      cb(null, Buffer.concat(buffers))
-    })
-  }
-}
+    // If the length is Infinity (i.e. a length was not specified) then the store will
+    // automatically grow.
 
-Storage.prototype.close = function (cb) {
-  const self = this
-  if (self.closed) return nextTick(cb, new Error('Storage is closed'))
-  self.closed = true
+    if (this.length !== Infinity) {
+      this.lastChunkLength = (this.length % this.chunkLength) || this.chunkLength
+      this.lastChunkIndex = Math.ceil(this.length / this.chunkLength) - 1
 
-  const tasks = self.files.map(function (file) {
-    return function (cb) {
-      file.open(function (err, file) {
-        // an open error is okay because that means the file is not open
-        if (err) return cb(null)
-        file.close(cb)
+      this.files.forEach(file => {
+        const fileStart = file.offset
+        const fileEnd = file.offset + file.length
+
+        const firstChunk = Math.floor(fileStart / this.chunkLength)
+        const lastChunk = Math.floor((fileEnd - 1) / this.chunkLength)
+
+        for (let p = firstChunk; p <= lastChunk; ++p) {
+          const chunkStart = p * this.chunkLength
+          const chunkEnd = chunkStart + this.chunkLength
+
+          const from = (fileStart < chunkStart) ? 0 : fileStart - chunkStart
+          const to = (fileEnd > chunkEnd) ? this.chunkLength : fileEnd - chunkStart
+          const offset = (fileStart > chunkStart) ? 0 : chunkStart - fileStart
+
+          if (!this.chunkMap[p]) this.chunkMap[p] = []
+
+          this.chunkMap[p].push({
+            from,
+            to,
+            offset,
+            file
+          })
+        }
       })
     }
-  })
-  parallel(tasks, cb)
-}
+  }
 
-Storage.prototype.destroy = function (cb) {
-  const self = this
-  self.close(function () {
-    if (self.path) {
-      fs.rm(self.path, { recursive: true, maxRetries: 10 }, err => {
-        err && err.code === 'ENOENT' ? cb() : cb(err)
+  put (index, buf, cb = () => {}) {
+    if (this.closed) return nextTick(cb, new Error('Storage is closed'))
+
+    const isLastChunk = (index === this.lastChunkIndex)
+    if (isLastChunk && buf.length !== this.lastChunkLength) {
+      return nextTick(cb, new Error('Last chunk length must be ' + this.lastChunkLength))
+    }
+    if (!isLastChunk && buf.length !== this.chunkLength) {
+      return nextTick(cb, new Error('Chunk length must be ' + this.chunkLength))
+    }
+
+    if (this.length === Infinity) {
+      this.files[0].open((err, file) => {
+        if (err) return cb(err)
+        file.write(index * this.chunkLength, buf, cb)
       })
     } else {
-      const tasks = self.files.map(function (file) {
-        return function (cb) {
-          fs.rm(file.path, { recursive: true, maxRetries: 10 }, err => {
-            err && err.code === 'ENOENT' ? cb() : cb(err)
+      const targets = this.chunkMap[index]
+      if (!targets) return nextTick(cb, new Error('no files matching the request range'))
+      const tasks = targets.map(target => {
+        return cb => {
+          target.file.open((err, file) => {
+            if (err) return cb(err)
+            file.write(target.offset, buf.slice(target.from, target.to), cb)
           })
         }
       })
       parallel(tasks, cb)
     }
-  })
+  }
+
+  get (index, opts, cb) {
+    if (typeof opts === 'function') return this.get(index, null, opts)
+    if (this.closed) return nextTick(cb, new Error('Storage is closed'))
+
+    const chunkLength = (index === this.lastChunkIndex)
+      ? this.lastChunkLength
+      : this.chunkLength
+
+    const rangeFrom = (opts && opts.offset) || 0
+    const rangeTo = (opts && opts.length) ? rangeFrom + opts.length : chunkLength
+
+    if (rangeFrom < 0 || rangeFrom < 0 || rangeTo > chunkLength) {
+      return nextTick(cb, new Error('Invalid offset and/or length'))
+    }
+
+    if (this.length === Infinity) {
+      if (rangeFrom === rangeTo) return nextTick(cb, null, Buffer.from(0))
+      this.files[0].open((err, file) => {
+        if (err) return cb(err)
+        const offset = (index * this.chunkLength) + rangeFrom
+        file.read(offset, rangeTo - rangeFrom, cb)
+      })
+    } else {
+      let targets = this.chunkMap[index]
+      if (!targets) return nextTick(cb, new Error('no files matching the request range'))
+      if (opts) {
+        targets = targets.filter(target => {
+          return target.to > rangeFrom && target.from < rangeTo
+        })
+        if (targets.length === 0) {
+          return nextTick(cb, new Error('no files matching the requested range'))
+        }
+      }
+      if (rangeFrom === rangeTo) return nextTick(cb, null, Buffer.from(0))
+
+      const tasks = targets.map(target => {
+        return cb => {
+          let from = target.from
+          let to = target.to
+          let offset = target.offset
+
+          if (opts) {
+            if (to > rangeTo) to = rangeTo
+            if (from < rangeFrom) {
+              offset += (rangeFrom - from)
+              from = rangeFrom
+            }
+          }
+
+          target.file.open((err, file) => {
+            if (err) return cb(err)
+            file.read(offset, to - from, cb)
+          })
+        }
+      })
+
+      parallel(tasks, (err, buffers) => {
+        if (err) return cb(err)
+        cb(null, Buffer.concat(buffers))
+      })
+    }
+  }
+
+  close (cb) {
+    if (this.closed) return nextTick(cb, new Error('Storage is closed'))
+    this.closed = true
+
+    const tasks = this.files.map(file => {
+      return cb => {
+        file.open((err, file) => {
+          // an open error is okay because that means the file is not open
+          if (err) return cb(null)
+          file.close(cb)
+        })
+      }
+    })
+    parallel(tasks, cb)
+  }
+
+  destroy (cb) {
+    this.close(() => {
+      if (this.path) {
+        fs.rm(this.path, { recursive: true, maxRetries: 10 }, err => {
+          err && err.code === 'ENOENT' ? cb() : cb(err)
+        })
+      } else {
+        const tasks = this.files.map(file => {
+          return cb => {
+            fs.rm(file.path, { recursive: true, maxRetries: 10 }, err => {
+              err && err.code === 'ENOENT' ? cb() : cb(err)
+            })
+          }
+        })
+        parallel(tasks, cb)
+      }
+    })
+  }
 }
 
-function nextTick (cb, err, val) {
-  queueMicrotask(function () {
-    if (cb) cb(err, val)
-  })
+function nextTick (cb = () => {}, err, val) {
+  queueMicrotask(() => cb(err, val))
 }
 
-function noop () {}
+module.exports = Storage
